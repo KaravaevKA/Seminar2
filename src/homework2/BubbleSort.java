b4a3138130acadf8a8e8a41c6093dcf613e2f3ac
Kirill Karavaev <coffee.2016@yandex.ru>
2023-03-31 19:44:29 +0300

    Homework Task1

---
+++ src/homework2/BubbleSort.java
@@ -0,0 +1,31 @@
+package homework2;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Random;
+
+public class BubbleSort {
+    public static void getSortedNum() {
+        boolean flag = false;
+        int buf = 0;
+        int[] arr = new int[]{6, 2, 5, 4, 2, 9, 10};
+        try {
+            FileWriter fw = new FileWriter("bubble.txt");
+            while (!flag) {
+                flag = true;
+                for (int i = 0; i < arr.length - 1; i++) {
+                    if (arr[i] > arr[i + 1]) {
+                        flag = false;
+                        buf = arr[i];
+                        arr[i] = arr[i + 1];
+                        arr[i + 1] = buf;
+                    }
+                    fw.append(Arrays.toString(arr) + "\n");
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/lesson2/Main.java
+++ src/lesson2/Main.java
@@ -1,5 +1,6 @@
 package lesson2;
 
+import homework2.BubbleSort;
 import task.Task1;
 import task.Task2;
 import task.Task3;
@@ -9,9 +10,6 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-//        System.out.printf("%s", Task1.getRepeatString(6));
-//        System.out.println(Task2.stringCompression("aaaabbbcdda"));
-        Task3.write("TEST");
     }
 }
 
